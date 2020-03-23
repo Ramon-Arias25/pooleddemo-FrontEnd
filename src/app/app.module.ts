8f6357ba4ff2ed4b40bf26ec7a5362ee51376d30
Ramon-Arias25 <56901379+Ramon-Arias25@users.noreply.github.com>
2020-03-23 13:48:37 -0300

    models frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routing, appRoutingProviders } from './app.routing';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/models/follow.ts
@@ -0,0 +1,7 @@
+export class Follow{
+    constructor(
+        public Id: string,
+        public user: string,
+        public followed: string
+    ){}
+}
